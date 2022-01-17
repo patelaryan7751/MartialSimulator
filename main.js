389e03fb7f657d496c5ee5ee7527c5610b2db21f
ARYAN PATEL <patelaryan7751@gmail.com>
2022-01-18 01:04:49 +0530

    update

--- main.js
+++ main.js
@@ -14,27 +14,39 @@ let frames = {
   idle: [1, 2, 3, 4, 5, 6, 7, 8],
   kick: [1, 2, 3, 4, 5, 6, 7],
   punch: [1, 2, 3, 4, 5, 6, 7],
+  forward: [1, 2, 3, 4, 5, 6],
+  backward: [1, 2, 3, 4, 5, 6],
+  block: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 };
 
 let loadImages = (callback) => {
-  let images = { idle: [], kick: [], punch: [] };
+  let images = {
+    idle: [],
+    kick: [],
+    punch: [],
+    forward: [],
+    backward: [],
+    block: [],
+  };
   let imagesToLoad = 0;
 
-  ["idle", "kick", "punch"].forEach((animation) => {
-    let animationFrames = frames[animation];
-    imagesToLoad = imagesToLoad + animationFrames.length;
-    animationFrames.forEach((frameNumber) => {
-      let path = imagePath(frameNumber, animation);
+  ["idle", "kick", "punch", "forward", "backward", "block"].forEach(
+    (animation) => {
+      let animationFrames = frames[animation];
+      imagesToLoad = imagesToLoad + animationFrames.length;
+      animationFrames.forEach((frameNumber) => {
+        let path = imagePath(frameNumber, animation);
 
-      loadImage(path, (image) => {
-        images[animation][frameNumber - 1] = image;
-        imagesToLoad = imagesToLoad - 1;
-        if (imagesToLoad === 0) {
-          callback(images);
-        }
+        loadImage(path, (image) => {
+          images[animation][frameNumber - 1] = image;
+          imagesToLoad = imagesToLoad - 1;
+          if (imagesToLoad === 0) {
+            callback(images);
+          }
+        });
       });
-    });
-  });
+    }
+  );
 };
 let animate = (ctx, images, animation, callback) => {
   images[animation].forEach((image, index) => {
@@ -63,6 +75,15 @@ loadImages((images) => {
   document.getElementById("punch").onclick = () => {
     queuedAnimations.push("punch");
   };
+  document.getElementById("forward").onclick = () => {
+    queuedAnimations.push("forward");
+  };
+  document.getElementById("backward").onclick = () => {
+    queuedAnimations.push("backward");
+  };
+  document.getElementById("block").onclick = () => {
+    queuedAnimations.push("block");
+  };
 
   document.addEventListener("keyup", (event) => {
     const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
@@ -70,6 +91,12 @@ loadImages((images) => {
       queuedAnimations.push("kick");
     } else if (key === "ArrowRight") {
       queuedAnimations.push("punch");
+    } else if (key === "ArrowUp") {
+      queuedAnimations.push("forward");
+    } else if (key === "ArrowDown") {
+      queuedAnimations.push("backward");
+    } else if (key === "Enter") {
+      queuedAnimations.push("block");
     }
   });
 });
